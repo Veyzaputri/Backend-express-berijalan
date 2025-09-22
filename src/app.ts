f63370488592e1eaf5e156cf8e549e2655a58154
Veyzaputri <129712217+Veyzaputri@users.noreply.github.com>
2025-09-22 13:36:06 +0700

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express from "express";
 import cors from "cors";
+import dotenv from "dotenv";
 import authRoutes from './routes/auth.route.js';
+import counterRoutes from "./routes/counter.route.js";
+import queueRoutes from "./routes/queue.route.js"
 import {MErrorHandler} from "./middlewares/error.middleware.js";
+import {connectRedis} from "./configs/redis.config.js";
 
+connectRedis();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use("/api/v1/auth", authRoutes);
+app.use("/api/counters", counterRoutes);
+app.use("/api/queue", queueRoutes);
 app.use(MErrorHandler);
 
 
